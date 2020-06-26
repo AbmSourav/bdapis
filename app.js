5fe60e3d7084a1c2285810ffbf88448980f8f608
AbmSourav <keramotul.islam@gmail.com>
2020-06-27 00:56:58 +0600

    mongodb connected

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
+const db_config = require('./db_config')
 
 const app = express()
+
+mongoose.connect(db_config.db_config, {useNewUrlParser: true});
+mongoose.connection
+  .once( 'open', () => console.log("connected") )
+  .on( 'error', (error) => console.log(error) );
 
 app
   .get('/', (req, res) => {
