73f6b6c66e81c782b88d2779b5be81a5f2c47fd6
AbmSourav <keramotul.islam@gmail.com>
2020-06-29 19:44:00 +0600

    heroku config var added

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 const db_config = require('./db_config')
 
+const dbConfig = process.env.DB_URL || db_config;
+
 const dbConnect = () => {
-    mongoose.connect(db_config, {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect(dbConfig, {useNewUrlParser: true, useUnifiedTopology: true});
     mongoose.connection
         .once( 'open', () => console.log("connected") )
         .on( 'error', error => console.log('error') );
