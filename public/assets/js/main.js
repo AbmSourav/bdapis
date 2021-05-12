dc0c8ce35d05a73a0d28e62508dbc15b082c7c34
AbmSourav <keramotul.islam@gmail.com>
2021-05-12 17:39:08 +0600

    homepage styles updated

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,25 +1,56 @@
 document.addEventListener("DOMContentLoaded", bdapi);
 
 function bdapi() {
-	const selectRoute = document.getElementById('select-route');
+	apiVersion();
+}
+
+function apiVersion() {
+	const selectedBtn = document.getElementById('selected-version');
+	const dropdown = document.getElementById('dropdown');
 	const templates = document.querySelectorAll('.route-template');
 	const inputElement = document.getElementById('route-doc-input');
 
 	templates.forEach( function(template, index) {
 		const templateContent = template.content.cloneNode(true);
-		if ( selectRoute.value == template.dataset.version) {
+		if ( selectedBtn.textContent == template.dataset.version) {
 			inputElement.appendChild(templateContent);
 		}
 	});
 
-	selectRoute.addEventListener('click', function(e) {
-		inputElement.innerHTML = '';
+	selectedBtn.addEventListener('click', function(e) {
+		if ( dropdown.style.display == 'none' ) {
+			dropdown.style.display = 'block';
+			this.classList.add('show');
+		} else {
+			dropdown.style.display = 'none';
+			this.classList.remove('show');
+		}
 
-		templates.forEach( function(template, index) {
-			const templateContent = template.content.cloneNode(true);
-			if ( template.dataset.version ==  e.target.value) {
-				inputElement.appendChild(templateContent);
-			}
+		selectOption();
+	})
+}
+
+function selectOption() {
+	const selectOption = document.querySelectorAll('.select-version-option');
+	const templates = document.querySelectorAll('.route-template');
+	const inputElement = document.getElementById('route-doc-input');
+	const dropdown = document.getElementById('dropdown');
+	const selectedBtn = document.getElementById('selected-version');
+
+	selectOption.forEach( function(option, index) {
+		option.addEventListener('click', function(e) {
+			inputElement.innerHTML = '';
+	
+			templates.forEach( function(template, index) {
+				const templateContent = template.content.cloneNode(true);
+				if ( template.dataset.version ==  option.dataset.version) {
+					inputElement.appendChild(templateContent);
+				}
+			});
+
+			selectedBtn.textContent = option.dataset.version;
+			selectedBtn.classList.remove('show');
+			dropdown.style.display = 'none';
 		})
-	})	
+	} );
 }
